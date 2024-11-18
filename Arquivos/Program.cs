176976bc28e86b13c004b519cf6b36d10c4a4a0d
Sávio Romário <savioromario10@gmail.com>
2024-11-18 11:26:49 -0300

    arquivos

---
+++ Arquivos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Arquivos
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string soucePath = @"c:\temp\file1.txt";
+            string targetPath = @"c:\temp\file2.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(soucePath);
+                fileInfo.CopyTo(targetPath);
+
+                string[] lines = File.ReadAllLines(soucePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Um erro ocorreu");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
